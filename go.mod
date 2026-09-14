8bb84a2c010682df35f846e206e2075deb2b4c74
Myself-Praveen <praveen104685@gmail.com>
2026-09-14 18:42:05 +0530

    feat: implement core WebSocket upgrader and handler

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Myself-Praveen/StreamMesh
 go 1.26.5
 
 require (
+	github.com/gorilla/websocket v1.5.3
 	github.com/spf13/viper v1.21.0
 	go.uber.org/zap v1.28.0
 )
---
+++ internal/ws/upgrader.go
@@ -0,0 +1,26 @@
+package ws
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	// Allow all origins for now, should be configured for production
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+	EnableCompression: true,
+}
+
+// UpgradeHandler upgrades HTTP requests to WebSocket connections
+func UpgradeHandler(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
